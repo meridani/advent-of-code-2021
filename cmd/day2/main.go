7d5341c967d20e1ec1f5b17655c1799532ac70ae
meridani <szmanndani@gmail.com>
2021-12-02 07:51:47 +0100

    Day2 done

---
+++ cmd/day2/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/meridani/advent-of-code-2021/pkg"
+	"github.com/meridani/advent-of-code-2021/pkg/execute"
+)
+
+var DAY = 2
+
+type Direction int
+
+const (
+	Forward = iota
+	Up
+	Down
+)
+
+func run(input pkg.Input) (interface{}, interface{}) {
+
+	// numbers := input.AsIntSlice()
+	lines := input.AsStringSlice()
+
+	horizontalPos, verticalPos, aim := 0, 0, 0
+	depthWithAim := 0
+
+	for _, line := range lines {
+		parts := strings.Split(strings.TrimSpace(line), " ")
+		value, err := pkg.MustAtoi(parts[1])
+		if err != nil {
+			return 0, 0
+		}
+
+		switch parts[0] {
+		case "forward":
+			horizontalPos += value
+			depthWithAim += value * aim
+		case "up":
+			verticalPos -= value
+			aim -= value
+		case "down":
+			verticalPos += value
+			aim += value
+		}
+	}
+
+	part1 := horizontalPos * verticalPos
+	part2 := depthWithAim * horizontalPos
+
+	return part1, part2
+}
+
+func main() {
+	pkg.CheckAndDownloadFile("input.txt", fmt.Sprintf("https://adventofcode.com/2021/day/%v/input", DAY))
+	input, err := pkg.ReadInput("input.txt")
+	if err != nil {
+		log.Fatal("Input file does not exists")
+	}
+	execute.Run(run, input)
+}
---
+++ cmd/day2/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/meridani/advent-of-code-2021/pkg"
+)
+
+var RunTests = []struct {
+	name         string
+	input        pkg.Input
+	want1, want2 int
+}{
+	{name: "No input", input: pkg.Input(""), want1: 0, want2: 0},
+	{name: "Example 1", input: pkg.Input(`forward 5
+	down 5
+	forward 8
+	up 3
+	down 8
+	forward 2`), want1: 150, want2: 900},
+}
+
+func TestRun(t *testing.T) {
+	for _, test := range RunTests {
+		got1, got2 := run(test.input)
+		if got1 != test.want1 {
+			t.Errorf("%v part 1 failed: got %v want %v", test.name, got1, test.want1)
+		}
+		if got2 != test.want2 {
+			t.Errorf("%v part 2 failed: got %v want %v", test.name, got2, test.want2)
+		}
+	}
+
+}
+
+func BenchmarkRun(b *testing.B) {
+	input, _ := pkg.ReadInput("input.txt")
+	for n := 0; n < b.N; n++ {
+		run(input)
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
