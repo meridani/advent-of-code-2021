449036b845f18c0c8594c4a4bc4b1bb348c7d509
meridani <szmanndani@gmail.com>
2021-12-03 09:40:52 +0100

    Test day 3

---
+++ cmd/day3/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/meridani/advent-of-code-2021/pkg"
+)
+
+var RunTests = []struct {
+	name         string
+	input        pkg.Input
+	want1, want2 int
+}{
+	{name: "No input", input: pkg.Input(""), want1: 0, want2: 0},
+	{name: "Example 1", input: pkg.Input(`00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`), want1: 198, want2: 230},
+}
+
+func TestRun(t *testing.T) {
+	for _, test := range RunTests {
+		got1, got2 := run(test.input)
+		if got1 != test.want1 {
+			t.Errorf("%v part 1 failed: got %v want %v", test.name, got1, test.want1)
+		}
+		if got2 != test.want2 {
+			t.Errorf("%v part 2 failed: got %v want %v", test.name, got2, test.want2)
+		}
+	}
+
+}
+
+func BenchmarkRun(b *testing.B) {
+	input, _ := pkg.ReadInput("input.txt")
+	for n := 0; n < b.N; n++ {
+		run(input)
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
