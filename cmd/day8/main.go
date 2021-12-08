5279540ae0943e32231bbe4737865f5771f2f832
meridani <szmanndani@gmail.com>
2021-12-08 08:00:57 +0100

    Part 1 ok, part 2 in progress

--- cmd/day8/main.go
+++ cmd/day8/main.go
@@ -3,19 +3,72 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/gitchander/permutation"
 	"github.com/meridani/advent-of-code-2021/pkg"
 	"github.com/meridani/advent-of-code-2021/pkg/execute"
 )
 
-var DAY = 1
+var DAY = 8
+
+const (
+	DIGIT_1   = 2
+	DIGIT_4   = 4
+	DIGIT_7   = 3
+	DIGIT_8   = 7
+	DIGIT_069 = 6
+	DIGIT_235 = 5
+)
+
+func decodeDigit(digit string) int {
+	digit = strings.TrimSpace(digit)
+	switch len(digit) {
+	case DIGIT_1:
+		return 1
+	case DIGIT_4:
+		return 4
+	case DIGIT_7:
+		return 7
+	case DIGIT_8:
+		return 8
+	default:
+		return -1
+	}
+}
+
+func part1(input []string) int {
+	sum := 0
+	for _, line := range input {
+		parts := strings.Split(line, "|")
+		if len(parts) == 2 {
+			digits := strings.Fields(parts[1])
+			for _, number := range digits {
+				if decodeDigit(number) > 0 {
+					sum++
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func tryPermutation(input []string) {
+	for _, line := range input {
+		parts := strings.Split(" | ")
+		if len(parts) == 2 {
+			digits := strings.Fields(parts[0])
+			perm := permutation.New(permutation.StringSlice{digits})
+		}
+	}
+}
 
 func run(input pkg.Input) (interface{}, interface{}) {
 
 	// numbers := input.AsIntSlice()
-	// lines := input.AsStringSlice()
+	lines := input.AsStringSlice()
 
-	part1 := 0
+	part1 := part1(lines)
 	part2 := 0
 
 	return part1, part2
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/meridani/advent-of-code-2021
 go 1.17
 
 require github.com/cavaliercoder/grab v2.0.0+incompatible
+
+require github.com/gitchander/permutation v0.0.0-20210517125447-a5d73722e1b1 // indirect
