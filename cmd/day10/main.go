9b413fa4532972a7ae766b1b32bc4c6f3cc65369
meridani <szmanndani@gmail.com>
2021-12-09 23:14:06 +0100

    Day 10 prepare

---
+++ cmd/day10/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/meridani/advent-of-code-2021/pkg"
+	"github.com/meridani/advent-of-code-2021/pkg/execute"
+)
+
+var DAY = 10
+
+func run(input pkg.Input) (interface{}, interface{}) {
+
+	// numbers := input.AsIntSlice()
+	// lines := input.AsStringSlice()
+
+	part1 := 0
+	part2 := 0
+
+	return part1, part2
+}
+
+func main() {
+	pkg.CheckAndDownloadFile("input.txt", fmt.Sprintf("https://adventofcode.com/2021/day/%v/input", DAY))
+	input, err := pkg.ReadInput("input.txt")
+	if err != nil {
+		log.Fatal("Input file does not exists")
+	}
+	execute.Run(run, input)
+}
