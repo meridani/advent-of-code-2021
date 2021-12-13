2077b7c398984b6157d565486d6a67fcbbf196fa
meridani <szmanndani@gmail.com>
2021-12-13 08:31:04 +0100

    Day 13

--- cmd/day13/main.go
+++ cmd/day13/main.go
@@ -3,21 +3,123 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/meridani/advent-of-code-2021/pkg"
 	"github.com/meridani/advent-of-code-2021/pkg/execute"
 )
 
-var DAY = 1
+var DAY = 13
+
+type point struct {
+	x, y int
+}
+
+type instruction struct {
+	x, y int
+}
+
+func decodePaper(lines []string) ([]point, []instruction) {
+	points := []point{}
+	instructions := []instruction{}
+	for _, line := range lines {
+		if len(line) < 3 {
+			continue
+		}
+		parts := strings.Split(strings.TrimSpace(line), ",")
+		if len(parts) == 2 {
+			x := pkg.MustAtoi(parts[0])
+			y := pkg.MustAtoi(parts[1])
+			points = append(points, point{x, y})
+		} else {
+			parts = strings.Split(line, " ")
+			parts = strings.Split(parts[2], "=")
+			num := pkg.MustAtoi(parts[1])
+			if parts[0] == "x" {
+				instructions = append(instructions, instruction{num, 0})
+			} else {
+				instructions = append(instructions, instruction{0, num})
+			}
+		}
+	}
+	return points, instructions
+}
+
+func fold(points *[]point, instructions *[]instruction) {
+	maxX, maxY := 0, 0
+	for _, p := range *points {
+		if p.x > maxX {
+			maxX = p.x
+		}
+		if p.y > maxY {
+			maxY = p.y
+		}
+	}
+
+	newpoints := []point{}
+	in := (*instructions)[0]
+	*instructions = (*instructions)[1:]
+
+	for _, p := range *points {
+		dx, dy := 0, 0
+		np := point{}
+		if in.x != 0 && p.x > in.x {
+			dx = in.x - p.x
+			np = point{in.x + dx, p.y}
+		} else if in.y != 0 && p.y > in.y {
+			dy = in.y - p.y
+			np = point{p.x, in.y + dy}
+		} else {
+			np = point{p.x, p.y}
+		}
+		if !contains(np, newpoints) {
+			newpoints = append(newpoints, np)
+		}
+	}
+	*points = newpoints
+}
+
+func contains(p point, points []point) bool {
+	for _, c := range points {
+		if c.x == p.x && c.y == p.y {
+			return true
+		}
+	}
+	return false
+}
 
 func run(input pkg.Input) (interface{}, interface{}) {
 
 	// numbers := input.AsIntSlice()
-	// lines := input.AsStringSlice()
+	lines := input.AsStringSlice()
 
 	part1 := 0
 	part2 := 0
-
+	points, instructions := decodePaper(lines)
+	fold(&points, &instructions)
+	part1 = len(points)
+	for range instructions {
+		fold(&points, &instructions)
+	}
+	maxX, maxY := 0, 0
+	for _, p := range points {
+		if p.x > maxX {
+			maxX = p.x
+		}
+		if p.y > maxY {
+			maxY = p.y
+		}
+	}
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			if contains(point{x, y}, points) {
+				fmt.Print("█")
+			} else {
+				fmt.Print("░")
+			}
+		}
+		fmt.Println()
+	}
 	return part1, part2
 }
 
--- cmd/day13/main_test.go
+++ cmd/day13/main_test.go
@@ -12,7 +12,27 @@ var RunTests = []struct {
 	input        pkg.Input
 	want1, want2 int
 }{
-	{name: "Example 1", input: pkg.Input(``), want1: 0, want2: 0},
+	{name: "Example 1", input: pkg.Input(`6,10
+	0,14
+	9,10
+	0,3
+	10,4
+	4,11
+	6,0
+	6,12
+	4,1
+	0,13
+	10,12
+	3,4
+	3,0
+	8,4
+	1,10
+	2,14
+	8,10
+	9,0
+
+	fold along y=7
+	fold along x=5`), want1: 17, want2: 0},
 }
 
 func TestRun(t *testing.T) {
