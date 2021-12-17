54820e6ff5cd9a7f1c3c5350c56c9a1b433b89d3
meridani <szmanndani@gmail.com>
2021-12-17 16:16:27 +0100

    Day 16 still nok

--- cmd/day16/main.go
+++ cmd/day16/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
 
 	"github.com/meridani/advent-of-code-2021/pkg"
 	"github.com/meridani/advent-of-code-2021/pkg/execute"
@@ -12,133 +11,21 @@ import (
 
 var DAY = 16
 
-const (
-	literal = 4 // single binary number
-)
-
-const (
-	VERSION_OFF     = 0
-	VERSION_LEN     = 3
-	TYPE_OFF        = 3
-	TYPE_LEN        = 3
-	LENGTH_TYPE_OFF = 6
-	LENGTH_TYPE_LEN = 1
-	LENGTH_OFF      = 7
-	LENGTH_BITS     = 15
-	LENGTH_PACKETS  = 11
-	LITERAL_OFFSET  = 6
-)
-
-type bitsPacket struct {
-	version byte
-	tID     byte
-	literal uint64
-	ltID    byte
-	length  uint64
-	content []bitsPacket
+type Packet struct {
+	version    byte
+	typeID     byte
+	ltypeID    byte
+	literal    uint64
+	length     uint64
+	subpackets []Packet
 }
 
-func stringToNum(i string) uint64 {
-	num, _ := strconv.ParseUint(i, 2, 64)
-	return num
-}
-
-func decodePacket(binary string) (bitsPacket, int) {
-	packet := bitsPacket{}
-	if len(binary) < TYPE_OFF+TYPE_LEN {
-		return packet, -1
-	}
-	packet.version = byte(stringToNum(binary[VERSION_OFF : VERSION_OFF+VERSION_LEN]))
-	packet.tID = byte(stringToNum(binary[TYPE_OFF : TYPE_OFF+TYPE_LEN]))
-	i := 0
-	switch packet.tID {
-
-	case literal:
-		var numberBuilder strings.Builder
-		i = LITERAL_OFFSET
-		for {
-			numberBuilder.WriteString(binary[i+1 : i+5])
-			if len(binary) < i+5 {
-				return bitsPacket{}, -1
-			}
-			if binary[i] == '0' {
-				break
-			}
-			i += 5
-		}
-
-		var err error
-		packet.literal = stringToNum(numberBuilder.String())
-
-		if err != nil {
-			fmt.Println("invalid literal found!")
-		}
-		i += 5
-
-	default:
-		packet.ltID = byte(stringToNum(string(binary[LENGTH_TYPE_OFF])))
-		i = LENGTH_OFF
-		if packet.ltID == 0 {
-			packet.length = stringToNum(binary[i : i+LENGTH_BITS])
-			i += LENGTH_BITS
-		} else {
-			packet.length = stringToNum(binary[i : i+LENGTH_PACKETS])
-			i += LENGTH_PACKETS
-		}
-	}
-
-	return packet, i
-}
-
-func toBinaryString(line string) string {
-	var binary strings.Builder
-
-	for _, c := range line {
-
-		switch c {
-		case '0':
-			binary.WriteString("0000")
-		case '1':
-			binary.WriteString("0001")
-		case '2':
-			binary.WriteString("0010")
-		case '3':
-			binary.WriteString("0011")
-		case '4':
-			binary.WriteString("0100")
-		case '5':
-			binary.WriteString("0101")
-		case '6':
-			binary.WriteString("0110")
-		case '7':
-			binary.WriteString("0111")
-		case '8':
-			binary.WriteString("1000")
-		case '9':
-			binary.WriteString("1001")
-		case 'A':
-			binary.WriteString("1010")
-		case 'B':
-			binary.WriteString("1011")
-		case 'C':
-			binary.WriteString("1100")
-		case 'D':
-			binary.WriteString("1101")
-		case 'E':
-			binary.WriteString("1110")
-		case 'F':
-			binary.WriteString("1111")
-		}
-	}
-	return binary.String()
-}
-
-func sumPart1(packets []bitsPacket) int {
-	sum := 0
-	for _, p := range packets {
-		sum += int(p.version)
-	}
-	return sum
+type Stream struct {
+	scanner *bufio.Scanner
+	buff    string
+	backlog []uint64
+	max     int
+	cur     int
 }
 
 func run(input pkg.Input) (interface{}, interface{}) {
@@ -148,13 +35,6 @@ func run(input pkg.Input) (interface{}, interface{}) {
 
 	part1 := 0
 	part2 := 0
-
-	binary := toBinaryString(strings.TrimSpace(string(input)))
-
-	for {
-	}
-
-	fmt.Println(packets)
 
 	return part1, part2
 }
--- cmd/day16/main_test.go
+++ cmd/day16/main_test.go
@@ -37,29 +37,29 @@ func TestRun2(t *testing.T) {
 var BitPacketDecoderTests = []struct {
 	name  string
 	input string
-	want  bitsPacket
+	want  Packet
 }{
-	{"1", "D2FE28", bitsPacket{version: 6, tID: 4}},
-	{"2", "38006F45291200", bitsPacket{version: 1, tID: 6, ltID: 0, length: 27,
-		content: []bitsPacket{
-			{version: 6, tID: 4, literal: 10},
-			{version: 2, tID: 4, literal: 20},
+	{"1", "D2FE28", Packet{version: 6, typeID: 4}},
+	{"2", "38006F45291200", Packet{version: 1, typeID: 6, ltypeID: 0, length: 27,
+		subpackets: []Packet{
+			{version: 6, typeID: 4, literal: 10},
+			{version: 2, typeID: 4, literal: 20},
 		}}},
-	{"3", "EE00D40C823060", bitsPacket{version: 7, tID: 3, ltID: 1, length: 3,
-		content: []bitsPacket{
-			{version: 2, tID: 4, literal: 1},
-			{version: 4, tID: 4, literal: 2},
-			{version: 1, tID: 4, literal: 3},
+	{"3", "EE00D40C823060", Packet{version: 7, typeID: 3, ltypeID: 1, length: 3,
+		subpackets: []Packet{
+			{version: 2, typeID: 4, literal: 1},
+			{version: 4, typeID: 4, literal: 2},
+			{version: 1, typeID: 4, literal: 3},
 		}}},
-	{"4", "8A004A801A8002F478", bitsPacket{version: 4, tID: 1}},
-	{"5", "620080001611562C8802118E34", bitsPacket{version: 3, tID: 3}},
-	{"6", "C0015000016115A2E0802F182340", bitsPacket{version: 7, tID: 3}},
-	{"7", "A0016C880162017C3686B18A3D4780", bitsPacket{version: 7, tID: 3}},
+	{"4", "8A004A801A8002F478", Packet{version: 4, typeID: 1}},
+	{"5", "620080001611562C8802118E34", Packet{version: 3, typeID: 3}},
+	{"6", "C0015000016115A2E0802F182340", Packet{version: 7, typeID: 3}},
+	{"7", "A0016C880162017C3686B18A3D4780", Packet{version: 7, typeID: 3}},
 }
 
 func TestDecodePacket(t *testing.T) {
 	for _, test := range BitPacketDecoderTests {
-		binary := toBinaryString(test.input)
+		binary, _ := toBinaryString(test.input)
 		got, _ := decodePacket(binary)
 		if got.version != test.want.version {
 			t.Errorf("%v failed: got %v want %v", test.name, got, test.want)
